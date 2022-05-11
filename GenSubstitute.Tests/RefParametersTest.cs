6e3824181dc72fc9091af7a10f95002f2170a670
Sakari Bergen <sakari@yousician.com>
2022-05-11 21:56:51 +0300

    Make RefArg immutable by default

--- GenSubstitute.Tests/RefParametersTest.cs
+++ GenSubstitute.Tests/RefParametersTest.cs
@@ -1,3 +1,4 @@
+using System;
 using FluentAssertions;
 using Xunit;
 
@@ -11,7 +12,7 @@ public class RefParametersTest
     }
 
     [Fact]
-    public void InterfaceWithRefParameterMethod_CanBeMockedWithSpecificValue()
+    public void RefParameterMethod_CanBeMockedWithSpecificValue()
     {
         var builder = Gen.Substitute<IRefParams>().Build();
         
@@ -24,7 +25,7 @@ public class RefParametersTest
     }
     
     [Fact]
-    public void InterfaceWithRefParameterMethod_CanBeMockedAnyValue()
+    public void RefParameterMethod_CanBeMockedUsingAnyValue()
     {
         var builder = Gen.Substitute<IRefParams>().Build();
         
@@ -34,5 +35,66 @@ public class RefParametersTest
         int foo = 10;
         builder.Object.Modify(42, ref foo);
         foo.Should().Be(42);
+    }
+    
+    [Fact]
+    public void RefParameterMethod_ShouldNotChangeMatching_WhenValueModified()
+    {
+        var builder = Gen.Substitute<IRefParams>().Build();
+
+        builder.Configure.Modify(Arg.Any, 5)
+            .Configure((_, r) => r.Value = 10 * r);
+
+        var foo = 5;
+        builder.Object.Modify(default, ref foo);
+
+        var bar = 5;
+        builder.Object.Modify(default, ref bar);
+        
+        foo.Should().Be(50);
+        bar.Should().Be(50);
+    }
+    
+    [Fact]
+    public void RefParameterMethod_ShouldNotReceivedCalls_WhenValueModified()
+    {
+        var builder = Gen.Substitute<IRefParams>().Build();
+        
+        builder.Configure.Modify(Arg.Any, 5)
+            .Configure((_, r) => r.Value = 10 * r);
+        
+        var foo = 5;
+        builder.Object.Modify(default, ref foo);
+        
+        var bar = 5;
+        builder.Object.Modify(default, ref bar);
+
+        builder.Received.Modify(default, 5).Count.Should().Be(2);
+    }
+
+    [Fact]
+    public void ReceivedRefArg_ShouldNotBeModifiable()
+    {
+        var builder = Gen.Substitute<IRefParams>().Build();
+        int foo = 0;
+        builder.Object.Modify(default, ref foo);
+
+        var receivedCall = builder.Received.Modify(Arg.Any, Arg.Any)[0];
+        var modifyReceivedValue = () => receivedCall.Arg2.Value = 0;
+        modifyReceivedValue.Should().Throw<InvalidOperationException>().WithMessage("*immutable*");
+    }
+    
+    [Fact]
+    public void AnyRefArg_ShouldNotBeModifiable()
+    {
+        var modifyAnyArg = () => RefArg<int>.Any.Value = 10;
+        modifyAnyArg.Should().Throw<InvalidOperationException>().WithMessage("*immutable*");
+    }
+    
+    [Fact]
+    public void DefaultRefArg_ShouldNotBeModifiable()
+    {
+        var modifyAnyArg = () => RefArg<int>.Default.Value = 10;
+        modifyAnyArg.Should().Throw<InvalidOperationException>().WithMessage("*immutable*");
     }
 }
--- GenSubstitute/RefArg.cs
+++ GenSubstitute/RefArg.cs
@@ -9,11 +9,28 @@ namespace GenSubstitute
     /// <typeparam name="T"></typeparam>
     public class RefArg<T> : IEquatable<RefArg<T>>
     {
-        public T Value { get; set; }
+        private readonly bool _isImmutable;
+        private T _value;
 
-        public RefArg(T value) => Value = value;
-        
-        public static readonly RefArg<T> Default = new(default!); // TODO, what if this is modified?
+        public T Value
+        {
+            get => _value;
+            set
+            {
+                if (_isImmutable)
+                {
+                    throw new InvalidOperationException(
+                        $"You can not modify the value of an immutable {nameof(RefArg<T>)}! " +
+                        $"This is most likely a value in received calls, {nameof(Default)}, or {nameof(Any)}");
+                }
+
+                _value = value;
+            }
+        }
+
+        public RefArg(T value, bool isImmutable = true) => (_value, _isImmutable) = (value, isImmutable);
+
+        public static readonly RefArg<T> Default = new(default!);
         public static readonly RefArg<T> Any = new(default!);
 
         public static implicit operator RefArg<T>(T val) => new (val);
